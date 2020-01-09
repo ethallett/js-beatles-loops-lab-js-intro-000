7132302549292103d137a281753ec085a9349a70
Elizabeth T Hallett <ethallett@gmail.com>
2020-01-09 01:00:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (let i = 0; i < 4; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+   }
+  return array;
+}
+
+function johnLennonFacts(array){
+  let n = 0
+  while (array.length < 4){
+    n++
+    array[n] = `${array[n]}!!!`
+ }
+  return array
+
+}
+
+// function iLoveTheBeatles(number){
+//   var array = []
+//     do {array.push('I love the Beatles')}
+//     while (number < 15) {
+//
+//     }
+//
+//     return array
+// }
